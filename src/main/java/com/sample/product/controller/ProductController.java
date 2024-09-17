ae76855ac2f17a45030b03a21ec823d63aba8a44
Adil khan <atcs.adil@mbusa.com>
2024-09-17 11:42:47 +0530

    this is for jenkins

---
+++ src/main/java/com/sample/product/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.sample.product.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+Author Adil khan
+version: java18
+ */
+@RestController
+@RequestMapping("/api/test")
+public class ProductController {
+	@GetMapping("/user")
+	public String getUser() {
+		return "Hi I'm using Jenkins only for testing purpose,Thanks";
+	}
+
+}
---
+++ src/test/java/com/sample/product/controller/ProductControllerTest.java
@@ -0,0 +1,33 @@
+package com.sample.product.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+/*
+Author Adil khan
+version: java18
+ */
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+	 @Autowired
+	    private MockMvc mockMvc;
+
+	    @Test
+	    public void testGetUserCorrectResponse() throws Exception {
+	        mockMvc.perform(MockMvcRequestBuilders.get("/api/test/user"))
+	               .andExpect(MockMvcResultMatchers.status().isOk())
+	               .andExpect(MockMvcResultMatchers.content().string("Hi I'm using Jenkins, this is for only testing"));
+	    }
+	    @Test
+	    public void testIncorrectUrl() throws Exception {
+	        mockMvc.perform(MockMvcRequestBuilders.get("/api"))
+	               .andExpect(MockMvcResultMatchers.status().isNotFound());
+	    }
+
+}
